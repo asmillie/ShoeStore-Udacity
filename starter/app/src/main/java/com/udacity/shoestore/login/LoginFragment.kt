084db4df504cd192f2281875205c0a46cf224e81
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-05-26 14:54:40 -0400

    Position ui elements and add TODOs

---
+++ starter/app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -0,0 +1,44 @@
+package com.udacity.shoestore.login
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentLoginBinding
+
+class LoginFragment : Fragment() {
+
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_login,
+            container,
+            false
+        )
+
+        // TODO: Bind buttons
+
+        return binding.root
+    }
+
+    fun onSignup() {
+        // TODO: Navigate to Welcome Screen
+    }
+
+    fun onLogin() {
+        // TODO: Navigate to Welcome Screen
+    }
+}
---
+++ starter/app/src/main/java/com/udacity/shoestore/login/LoginViewModel.kt
@@ -0,0 +1,12 @@
+package com.udacity.shoestore.login
+
+import androidx.lifecycle.ViewModel
+
+class LoginViewModel: ViewModel() {
+
+    init {
+
+    }
+
+
+}
