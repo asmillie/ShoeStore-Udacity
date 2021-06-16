5125e55a296bed3823589eca815829f670b5a020
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-06-16 10:18:10 -0400

    Add RelativeLayout to position FAB bottom right

--- starter/app/src/main/java/com/udacity/shoestore/store/ShoeListFragment.kt
+++ starter/app/src/main/java/com/udacity/shoestore/store/ShoeListFragment.kt
@@ -1,15 +1,15 @@
 package com.udacity.shoestore.store
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 import com.udacity.shoestore.store.models.Shoe
@@ -30,7 +30,18 @@ class ShoeListFragment : Fragment() {
         initButton()
         initObservables()
 
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.main_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController()
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 
     private fun initButton() {
@@ -57,7 +68,7 @@ class ShoeListFragment : Fragment() {
         Timber.i("Adding view for shoe ${shoe.name}")
 
         val shoeText = TextView(context)
-        shoeText.text = "Name: ${shoe.name}, Size: ${shoe.size}, Company: ${shoe.company}, Description: ${shoe.description}"
+        shoeText.text = "${shoe.name}: Size ${shoe.size}, Company: ${shoe.company}, Description: ${shoe.description}"
         shoeText.layoutParams = LinearLayout.LayoutParams(
             LinearLayout.LayoutParams.MATCH_PARENT,
             LinearLayout.LayoutParams.WRAP_CONTENT
