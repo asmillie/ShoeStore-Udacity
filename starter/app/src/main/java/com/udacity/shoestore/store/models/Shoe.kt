36275c712403631e09d94137ed3254b6ece58807
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-06-16 11:23:18 -0400

    Flatten directory structure

--- starter/app/src/main/java/com/udacity/shoestore/store/models/Shoe.kt
+++
@@ -1,8 +0,0 @@
-package com.udacity.shoestore.store.models
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Shoe(var name: String, var size: Double, var company: String, var description: String,
-                val images: List<String> = mutableListOf()) : Parcelable
