7ec9e2627fe3c62ed54fab81365f69975b9fe98d
Alan Smillie <48022778+asmillie@users.noreply.github.com>
2021-06-17 10:58:05 -0400

    Use more concise binding call

--- app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
+++ app/src/main/java/com/udacity/shoestore/login/LoginFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
-import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment() {
@@ -19,9 +17,8 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        binding = DataBindingUtil.inflate(
+        binding = FragmentLoginBinding.inflate(
             inflater,
-            R.layout.fragment_login,
             container,
             false
         )
